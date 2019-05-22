bbd044337b1d5bc485d1b0cc918196b5810efb8f
Mathieu314 <mathieu.moneyron44@gmail.com>
2019-05-22 11:26:17 +0200

    Update node code

--- code/node/noeud.ino
+++ code/node/noeud.ino
@@ -5,16 +5,28 @@
 #include <SoftwareSerial.h>
 #include "Adafruit_SHT31.h"
 #include "SdsDustSensor.h"
+#include <LoRaWan.h>
+#include <EnergySaving.h>
+#include <RTCInt.h>
 
-int rxPin = 7;
-int txPin = 8;
+EnergySaving nrgSave;
+RTCInt rtc;
+
+int rxPin = 0;
+int txPin = 1;
+int delay_temp= 10000;
 SdsDustSensor sds(rxPin, txPin);
+String str_pm="-1 -1";
 
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
 String str;
 
-String command1="AT+CMSGHEX=";
+//String command1="AT+CMSGHEX=";
+String command1="AT+MSG=";
+
+void rest_alarm_int(void)  //interrupt routine
+{}
 
 int Get_Temperature(){
   float t = sht31.readTemperature();
@@ -35,81 +47,71 @@ int Get_Humidity(){
  }
 
 String Get_PM(){
+    sds.wakeup();
+    delay(30000);
     PmResult pm = sds.readPm();
     String pm25=String(int(pm.pm25));
     String pm10=String(int(pm.pm10));
-    String s=pm25+" "+pm10;
+    sds.sleep();
   if (pm.isOk()) {
-    return s;
-   //return pm.toString();
+    str_pm =pm25+" "+pm10;
+    return str_pm;
   } else {
-    return "-1 -1";
+    return str_pm;
   }
 }
-
-void Test_Commands(void){
-  /*
-  while(SerialUSB.available())
-     {
-         Serial1.write(SerialUSB.read());
-
-     }
-    */
-   while(Serial1.available())
-     {
-        SerialUSB.write(Serial1.read());
-     }
- }
 
 void Measure(){
   str=String(Get_Temperature())+" "+String(Get_Humidity())+ " "+ Get_PM();
   SerialUSB.println("Finish measure:"+str);
-  delay(5000);
+  delay(delay_temp);
 }
 
 void Envoyer(){
   String message=command1+"\""+str+"\"";
   Serial1.println(message);
   SerialUSB.println("Finish envoyer:"+message);
-  delay(5000);
-  //SerialUSB.println("Finish envoyer:"+message);
+  delay(delay_temp);
 }
-
 
  void setup()
  {
     Serial1.begin(9600);
     SerialUSB.begin(9600);
 
-     sds.begin();
+    sds.begin();
 
     SerialUSB.println("Start");
 
-  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
-    SerialUSB.println("Couldn't start");
-    while (1) delay(1);
-  }
+    if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+      SerialUSB.println("Couldn't start");
+      while (1) delay(1);
+    }
+
+    rtc.begin(TIME_H24);
+    rtc.setTime(0,0,0,0);
+    rtc.enableAlarm(SEC,ALARM_INTERRUPT,rest_alarm_int);
+    rtc.time.hour=0;
+    rtc.time.minute=0;
+    rtc.time.second=30;
+    nrgSave.begin(WAKE_RTC_ALARM);  //standby setup for external interrupts
+    rtc.setAlarm();
+
  }
 
 
  void loop()
  {
-  //SerialUSB.println(String(Get_Temperature())+"aaa");
-  //SerialUSB.println(String(Get_Humidity())+"bbbb");
-  //delay(3000);
-
-  delay(5000);    //delay 5s au debut
-  Serial1.println("at+mode=lwabp");
-  delay(5000);    //delay 5s
-  Test_Commands();
-
-  while(1){
-    Measure(); //delay 20s
-    Envoyer(); //delay 10s
-    Test_Commands();
-    delay(60000);
+    Measure();
+    Envoyer();
     SerialUSB.println("--------------------------------------");
-    // Serial1.println("AT+CMSGHEX=\"22.5\"");
-    //  Test_Commands();
-  }
+
+    rtc.begin(TIME_H24);
+    rtc.setTime(0,0,0,0);
+    rtc.enableAlarm(SEC,ALARM_INTERRUPT,rest_alarm_int);
+    rtc.time.hour=0;
+    rtc.time.minute=0;
+    rtc.time.second=30;
+    nrgSave.begin(WAKE_RTC_ALARM);  //standby setup for external interrupts
+    rtc.setAlarm();
 }
